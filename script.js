b9f338fe7a606bb90e331de444ab4f2dc81cd422
Rabia Bezzaz <r.bezzaz@yahoo.fr>
2022-05-22 13:03:32 +0200

    init

---
+++ script.js
@@ -0,0 +1,106 @@
+let video = document.querySelector('.video');
+let juice = document.querySelector('.orange-juice');
+let btn = document.getElementById('play-pause');
+let muteBtn = document.getElementById('mute');
+let volumeslider = document.getElementById('volumeSlider');
+let orangeBar = document.querySelector('.orange-bar');
+let fullScreen = document.getElementById('fullScreen');
+
+
+const togglePlayPause=()=>{
+    if(video.paused){
+        btn.setAttribute('class', 'pause');
+        video.play();
+    }
+    else {
+        btn.className = "play";
+        video.pause();
+    }
+    
+}
+
+
+btn.onclick = function(){
+    togglePlayPause();
+}
+
+// barre orange
+
+video.addEventListener('timeupdate', function(){
+
+    let juicePos = video.currentTime / video.duration;
+
+    juice.style.width = juicePos * 100 + '%';
+
+    if(video.ended) {
+        btn.className ="play";
+    }
+
+
+})
+
+// mute la video
+
+
+muteBtn.addEventListener('click', function(){
+
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerHTML = "Mute";
+    } else {
+        video.muted = true;
+        muteBtn.innerHTML = "Unmute";
+    }
+
+})
+
+// Volume
+
+
+volumeslider.addEventListener('change', function(){
+
+
+    video.volume = volumeslider.value / 100;
+
+})
+
+
+// la barre orange clikable
+
+let rect = orangeBar.getBoundingClientRect();
+console.log(rect);
+
+let largeur = rect.width;
+
+orangeBar.addEventListener('click', function(e){
+
+
+    // la valeur de notre click sur x par rapport a notre element
+
+    let x = e.clientX - rect.left;
+    
+    let widthPercent = ((x*100)/largeur);
+    
+    let currentTimeTrue = (widthPercent * 10 ) / 100;
+    
+    // position en secondes
+    video.currentTime = currentTimeTrue;
+    // barre orange qui va la ou on clique
+    juice.style.width = widthPercent + '%';
+
+})
+
+//position plein ecran : togglefullscreen
+
+
+ const toggleFullScreen=()=>{
+    if(video.requestFullScreen){
+        video.requestFullScreen();
+       } else if(video.webkitRequestFullScreen){
+        video.webkitRequestFullScreen();
+       } else if(video.mozRequestFullScreen){
+        video.mozRequestFullScreen();
+       }
+ } 
+
+ fullScreen.addEventListener('click', toggleFullScreen);
